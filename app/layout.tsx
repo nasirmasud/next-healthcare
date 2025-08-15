142620b0c4b570a8f07151e0650d6169a94e8479
Nasir <nasir.masud@ymail.com>
2025-08-15 21:02:00 +0600

    Warped children by Theme Provider of shadCN

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import { ThemeProvider } from "@/components/theme-provider";
 import { cn } from "@/lib/utils";
 import type { Metadata } from "next";
 import { Plus_Jakarta_Sans } from "next/font/google";
@@ -22,7 +23,9 @@ export default function RootLayout({
   return (
     <html lang='en'>
       <body className={cn("min-h-screen bg-dark-300 font-sans antialiased")}>
-        {children}
+        <ThemeProvider attribute='class' defaultTheme='system'>
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   );
