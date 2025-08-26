0c86ac012ec11b9ca9c21a77010f683d6e558742
Nasir <nasir.masud@ymail.com>
2025-08-26 06:09:49 +0600

    Refactor RootLayout component to add flexbox styling for improved layout alignment

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
     <html lang='en' suppressHydrationWarning>
       <body
         className={cn(
-          "min-h-screen bg-dark-300 font-sans antialiased",
+          "min-h-screen flex items-center justify-center bg-dark-300 font-sans antialiased",
           fontSans.variable
         )}
       >
